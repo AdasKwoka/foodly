245940f0ce2375aafb44ce43a66534e461ae3bb9
Adam Kwoka <AdamKwoka@theambassadors.com>
2022-12-03 17:11:50 +0100

    Add libraries and logo

---
+++ screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import { SafeAreaView, View, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+const HomeScreen = () => {
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    })
+  }, [])
+  return (
+    <SafeAreaView>  
+      <View>
+        <Image />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
